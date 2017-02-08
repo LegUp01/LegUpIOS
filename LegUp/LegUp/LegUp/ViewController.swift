be7539020dfad081073f86a2f8cea3a4c33b189e
Ayanna Kosoko <ayannakosoko@gmail.com>
2017-02-07 20:36:13 -0500

    legup project

---
+++ LegUp/LegUp/LegUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LegUp
+//
+//  Created by Ayanna Kosoko on 2/7/17.
+//  Copyright © 2017 Ayanna Kosoko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
