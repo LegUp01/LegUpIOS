6546c0cbcc7500aaa80f59f02547b67356bf1de3
Sean Morgan <seanmorgan@gmail.com>
2017-03-10 12:03:55 -0500

    connected to AWS

--- LegUp/LegUp/LegUp/ViewController.swift
+++ LegUp/LegUp/LegUp/ViewController.swift
@@ -7,18 +7,40 @@
 //
 
 import UIKit
+import AWSDynamoDB
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        dynamoTest()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func dynamoTest() {
+        let dynamodb = AWSDynamoDB.default()
+        let listTableInput = AWSDynamoDBListTablesInput()
+        dynamodb.listTables(listTableInput!).continueWith { (task) -> Any? in
+            if let error = task.error {
+                print("Error occurred: \(error)")
+                return nil
+            }
+            let listTablesOutput = task.result!
+            
+            for tableName in listTablesOutput.tableNames! {
+                //fetch each table name
+                print("\(tableName)")
+            }
+            return nil
+        }
+    }
+    
 
 
 }
